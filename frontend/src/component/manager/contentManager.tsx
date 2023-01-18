7227a5cd909fb74dc779be8b1711307528a7c41a
ThijsOnGitHub <47325246+ThijsOnGitHup@users.noreply.github.com>
2023-01-18 01:25:43 +0100

    Removed unused function

--- frontend/src/component/manager/contentManager.tsx
+++ frontend/src/component/manager/contentManager.tsx
@@ -1,6 +1,4 @@
 import {FC, useState} from "react";
-import {Slides, SlideType} from "../../types/Slides";
-import {useSlides} from "../../hooks/useSlides";
 
 export interface ContentManagerProps{
 
@@ -9,7 +7,6 @@ export interface ContentManagerProps{
 
 export const ContentManager: FC<ContentManagerProps> = (props) => {
     const index = useState<number>(0)
-    const slides = useSlides()
 
 
     return <div></div>
