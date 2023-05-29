2713263dc62fd64631d49912f05c3c6030cfbf2d
ThijsOnGitHub <47325246+ThijsOnGitHup@users.noreply.github.com>
2023-05-29 16:49:02 +0200

    Add selectedSlide to overview

---
+++ frontend/src/component/SelectedSlide.tsx
@@ -0,0 +1,72 @@
+import { renderSlide, translateTypes } from "../pages/Overview"
+import { Slide, SlideTypes } from "../types/Slides"
+import { FitToScreen } from "./slideUtilities/fitToScreen"
+
+export interface selectedSlideProps {
+    slide: Slide
+    onCompleted: () => void
+}
+
+
+export const SelectedSlide: React.FC<selectedSlideProps> = ({ slide, onCompleted }) => {
+
+    const renderData = () => {
+        switch (slide.type) {
+            case SlideTypes.POSTBLOCK:
+                return <div>
+                    Type: {translateTypes(slide.type)}
+                    <br />
+                    Aantal NiewsItems: {slide.slides.length}
+                    NiewsItems:
+                    <ul>
+                        {slide.slides.map((newsItem) => {
+                            return <li> {newsItem.title}
+                                <ul>
+                                    <li>Categorie: {newsItem.category.subject?.subject}</li>
+                                    {newsItem.endDate ? <li>Eind datum: {newsItem.endDate.toLocaleDateString()}</li> : null}
+                                    {newsItem.postImage ? [<li>Afbeelding lengte: {newsItem.imageLength} Seconden </li>,<li>Afbeelding: {newsItem.postImage}</li>] : null}
+                                    <li>Lengte: {newsItem.length} Seconden</li>
+                                </ul>
+                            </li>
+                        })}
+                    </ul>
+                </div>
+            case SlideTypes.IMAGE:
+                return <div>
+                    Type: {translateTypes(slide.type)}
+                    <br />
+                    Afbeelding: {slide.imageUrl}
+                    <br />
+                    Lengte: {slide.length} Seconden
+                </div>
+            case SlideTypes.TEXT_SLIDE:
+                return <div>
+                    Type: {translateTypes(slide.type)}
+                    <br />
+                    Titel: {slide.title}
+                    <br />
+                    Tekst: {slide.content}
+                    <br />
+                    Categorie: {slide.category?.subject?.subject}
+                    <br />
+                    Lengte: {slide.length} Seconden
+                    <br />
+                    Eind datum: {slide.endDate?.toLocaleDateString()}
+                </div>
+        }
+    }
+
+        
+
+    return (
+        <div>
+            <div style={{ background: "white" }}>
+                {renderData()}
+                <button onClick={onCompleted}>Terug</button>
+            </div>
+            <FitToScreen baseHeight={1080} baseWidth={1920}>
+                {renderSlide(slide, onCompleted)}
+            </FitToScreen>
+        </div>
+    )
+}
--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,21 +1,51 @@
 import { FC, useMemo, useState } from "react"
 import { useWordpressPostData } from "../hooks/useWordpressPostData"
 import { useWordpressSlides } from "../hooks/useWordpressSlides"
-import { SlideTypes } from "../types/Slides"
+import { Slide, SlideTypes } from "../types/Slides"
 import { PostBlockSlide } from "../component/slides/PostBlockSlide"
 import { ImageSlide } from "../component/slides/ImageSlide"
 import { NewsItem } from "../items/NewsItem"
 import { FitToScreen } from "../component/slideUtilities/fitToScreen"
 import { filterSlides } from "./Kabelkrant"
 import { format } from "date-fns"
+import { SelectedSlide } from "../component/SelectedSlide"
 
 export interface OverviewProps {
+}
+
+
+export const renderSlide = (slide: Slide, onComplete: ()=> void = ()=>{}) => {
+    switch (slide.type) {
+        case SlideTypes.POSTBLOCK:
+            return <PostBlockSlide posts={slide.slides} onCompleted={onComplete} />
+        case SlideTypes.IMAGE:
+            return <ImageSlide title={""} showText={false} backgroundImageURL={slide.imageUrl} length={slide.length} onCompleted={onComplete} />
+        case SlideTypes.TEXT_SLIDE:
+            return <NewsItem post={slide} nextSlide={onComplete} />
+        case SlideTypes.VOID:
+            return  <div style={{ color: "black" }}> </div>
+    }
+}
+
+export function translateTypes(type: SlideTypes){
+    switch(type){
+        case SlideTypes.POSTBLOCK:
+            return "Nieuws Items"
+        case SlideTypes.IMAGE:
+            return "Afbeelding"
+        case SlideTypes.TEXT_SLIDE:
+            return "Tekst Dia"
+        case SlideTypes.VOID:
+            return "Void" 
+    }
 }
 
 export const Overview: FC<OverviewProps> = (props) => {
     const { posts, categories } = useWordpressPostData()
     const { slides } = useWordpressSlides(posts, categories)
     const [date, setDate] = useState(new Date())
+    const [selectedSlide, setSelectedSlide] = useState<Slide | null>()
+
 
     const filteredSlides = useMemo( ()=> filterSlides(slides, date).map((slide) => {
       if(slide.type === SlideTypes.POSTBLOCK){
@@ -35,17 +65,8 @@ export const Overview: FC<OverviewProps> = (props) => {
     }),[slides,date])
 
 
-    function translateTypes(type: SlideTypes){
-        switch(type){
-            case SlideTypes.POSTBLOCK:
-                return "Nieuws Items"
-            case SlideTypes.IMAGE:
-                return "Afbeelding"
-            case SlideTypes.TEXT_SLIDE:
-                return "Tekst Dia"
-            case SlideTypes.VOID:
-                return "Void" 
-        }
+    if(selectedSlide != null){
+        return <SelectedSlide slide={selectedSlide} onCompleted={() => setSelectedSlide(null)} />
     }
 
     return (
@@ -56,26 +77,11 @@ export const Overview: FC<OverviewProps> = (props) => {
             </div>
             <div style={{ display: "flex", flexWrap: "wrap", gap: 20, padding: 30 }}>
                 {filteredSlides.map((slide) => {
-                    console.log("Rendering slide", slide)
-                    let element = <div></div>
-                    switch (slide.type) {
-                        case SlideTypes.POSTBLOCK:
-                            element = <PostBlockSlide posts={slide.slides} onCompleted={() => { }} />
-                            break;
-                        case SlideTypes.IMAGE:
-                            element = <ImageSlide title={""} showText={false} backgroundImageURL={slide.imageUrl} length={slide.length} onCompleted={() => { }} />
-                            break;
-                        case SlideTypes.TEXT_SLIDE:
-                            element = <NewsItem post={slide} nextSlide={() => { }} />
-                            break;
-                        case SlideTypes.VOID:
-                            element = <div style={{ color: "black" }}> </div>
-                            break;
-                    }
-                    return <div style={{overflow: "hidden", background: "white", color: "black",borderRadius: 10,width: 340}}> 
-                        <div style={{ background: "black"  }}>
+                    console.log("Rendering slide", slide) 
+                    return <div style={{overflow: "hidden", background: "white", color: "black",borderRadius: 10,width: 340, cursor: "pointer"}} onClick={()=>setSelectedSlide(slide)}> 
+                        <div style={{ background: "black",  }} >
                             <FitToScreen rerender={false} element={"container"} baseHeight={1080} baseWidth={1920}  >{
-                                element
+                                renderSlide(slide, () => {})
                             }</FitToScreen>
                         </div>
                         <div style={{padding: 10}}>
