78446c4a45f95032d672bb77b50bd47e87799bd5
ThijsOnGitHub <thijsgeurts1@gmail.com>
2024-03-07 21:45:04 +0100

    [feature] Add support for interval to useTimer

--- frontend/src/hooks/utilities/useTimer.tsx
+++ frontend/src/hooks/utilities/useTimer.tsx
@@ -7,17 +7,19 @@ import {useCallback, useEffect, useMemo, useState} from "react";
  * @param intervalDuration How often the timer should update in miliseconds
  */
 
-export function useTimer(maxSeconds: number, onComplete?: ()=>void, intervalDuration = 10,tag?: string) {
+export function useTimer(maxSeconds: number | false, onComplete?: ()=>void, intervalDuration = 10,tag?: string) {
     const [seconds, setSeconds] = useState(0)
     const [done, setDone] = useState(false)
     const [date, setDate] = useState(new Date())
     const [isRunning, setIsRunning] = useState(false)
 
     function stopTimer() {
+        console.log("Stopping timer", tag)
         setIsRunning(false)
     }
 
     const resetAndStartTimer = () => {
+        console.log("Resetting timer", tag)
         setDate(new Date())
         setDone(false)
         setIsRunning(true)
@@ -30,7 +32,7 @@ export function useTimer(maxSeconds: number, onComplete?: ()=>void, intervalDura
     },[done])
 
     useEffect(() => {
-        if (seconds>= maxSeconds) {
+        if (maxSeconds !== false && seconds>= maxSeconds) {
             setIsRunning(false)
             setDone(true)
         }
@@ -38,10 +40,15 @@ export function useTimer(maxSeconds: number, onComplete?: ()=>void, intervalDura
 
 
     useEffect(() => {
+        console.log("Starting timer",isRunning, tag)
         if(!isRunning) return
         const interval = setInterval(async () => {
+            console.log("Interval", tag)
             const secondsDone = (new Date().getTime() - date.getTime()) / 1000
             setSeconds(secondsDone)
+            if(maxSeconds === false) {
+                onComplete?.()
+            }
         }, intervalDuration)
         return () => {
             clearInterval(interval)
