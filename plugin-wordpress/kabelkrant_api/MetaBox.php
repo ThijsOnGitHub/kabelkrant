ef03013b37de1ac0af39c3663c4fc673b3653c1d
ThijsOnGitHub <thijsgeurts1@gmail.com>
2024-03-17 21:43:32 +0100

    [feature] Add live update data

--- plugin-wordpress/kabelkrant_api/MetaBox.php
+++ plugin-wordpress/kabelkrant_api/MetaBox.php
@@ -7,9 +7,7 @@ class MetaBox{
     }
 
     function wpdocs_register_meta_boxes() {
-        if (get_field('show_on_tv')){
-            add_meta_box( 'meta-box-id', __( 'Kabelkrant preview', 'textdomain' ), [$this,'wpdocs_my_display_callback'], 'post', 'normal', 'high');
-        }
+        add_meta_box( 'meta-box-id', __( 'Kabelkrant preview', 'textdomain' ), [$this,'wpdocs_my_display_callback'], 'post', 'normal', 'high');
     }
 
     
@@ -41,7 +39,11 @@ class MetaBox{
                 break;
         }
     
-        ?> <iframe src="<?php echo $preview_url ?>" id="kabelkrant-preview" style="aspect-ratio:16/10;width:100%" ></iframe>   <?php
+        ?> 
+            
+            <iframe src="<?php echo $preview_url ?>" id="kabelkrant-preview" style="aspect-ratio:16/10;width:100%" ></iframe>   
+            <div id="kabelkrant-disabled" >Deze post wordt niet op de kabelkrant weergegeven</div>
+        <?php
          if (false){
             echo json_encode($value);
             echo json_encode($fieldOfCategory);	
@@ -61,8 +63,70 @@ class MetaBox{
             ],
         ];
         $encoded_data = json_encode($data);
+
         ?> <script>
             var iframe = document.getElementById('kabelkrant-preview');
+            var warning = document.getElementById('kabelkrant-disabled');
+            let acfField = <?php echo $encoded_data; ?>;
+
+            function updatePreviewShow(showPreview){
+                if(showPreview){
+                    iframe.style.display = "block";
+                    warning.style.display = "none";
+                } else {
+                    iframe.style.display = "none";
+                    warning.style.display = "block";
+                }
+            }
+
+
+            acf.add_action('ready', function() {
+                const isOnTV = acf.getField('field_63daae2e861d2');
+
+                
+                // get input element 
+                var title =document.getElementById('title')
+                title.addEventListener('input', function(){
+                    acfField.post.title.rendered = title.value;
+                    let data = {'type': "new_preview_data", data: acfField }
+                    iframe.contentWindow.postMessage( data , '*');
+                });
+
+
+                // updat ecf title 
+                var titleAcfField = acf.getField('field_6474bd77ab7c0');
+                titleAcfField.on('change', function(){
+                    acfField.post.acf.tv_settings.title = titleAcfField.val();
+                    let data = {'type': "new_preview_data", data: acfField }
+                    iframe.contentWindow.postMessage( data , '*');
+                });
+
+
+
+                isOnTV.on('change', function(){
+                    updatePreviewShow(isOnTV.val());
+                });
+                updatePreviewShow(isOnTV.getValue())
+            });
+           
+
+ 
+            
+
+
+            acf.addAction('wysiwyg_tinymce_init', function( ed, id, mceInit, field ){
+                if(field.data.key  == "field_63daae1d861d1"){
+                    ed.on('change', function(){
+                        acfField.post.acf.tv_settings.text = ed.getContent();
+                        let data = {'type': "new_preview_data", data: acfField }
+                        iframe.contentWindow.postMessage( data , '*');
+                    });
+                }
+                // ed (object) tinymce object returned by the init function
+                // id (string) identifier for the tinymce instance
+                // mceInit (object) args given to the tinymce function
+                // field (object) field instance 
+            });
             iframe.onload = function() {
                 console.log('iframe loaded',<?php echo $encoded_data; ?>);
                 try{
@@ -75,6 +139,8 @@ class MetaBox{
                         iframe.contentWindow.postMessage( {'type': "new_preview_data", data : <?php echo $encoded_data; ?> } , '*');
                     }
                 });
+
+
             };
         </script> <?php
 
