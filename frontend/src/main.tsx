c1270d4f708d25ebd78ee55e78e37390000c4e9a
ThijsOnGitHub <thijsgeurts1@gmail.com>
2024-10-12 17:19:38 +0200

    [feature] Add sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
-import React, {StrictMode} from 'react'
+import * as Sentry from "@sentry/react";
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -6,6 +7,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
+Sentry.init({
+  dsn: "https://c92f04c49870bcf6a9635bf6986d89bc@o4506860416401408.ingest.us.sentry.io/4508110424440832",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,22 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "thijs-r2",
+    project: "kabelkrant"
+  })],
+
   css: {
     modules:{
         localsConvention: 'camelCaseOnly',
         scopeBehaviour: 'local'
     },
+  },
+  build: {
+    sourcemap: true
   }
 })
